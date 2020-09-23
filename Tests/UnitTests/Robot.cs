e8c9e6d84213a744d004516f508ea27e3aeaf6a1
Pau Mestre Martín <pau.mestre@turingchallenge.com>
2020-09-23 20:23:33 +0200

    Add new unit test for Robot rotation

--- Tests/UnitTests/Robot.cs
+++ Tests/UnitTests/Robot.cs
@@ -6,6 +6,34 @@ namespace Tests
     public class RobotTest
     {
         [Fact]
+        public void WhenRobotRotatesRightItsOrientationChangesAccordingly()
+        {
+            var robot = new Robot(new Position(1, 1, Orientation.N));
+            robot.RotateRight();
+            Assert.Equal(Orientation.E, robot.Position.Orientation);
+            robot.RotateRight();
+            Assert.Equal(Orientation.S, robot.Position.Orientation);
+            robot.RotateRight();
+            Assert.Equal(Orientation.W, robot.Position.Orientation);
+            robot.RotateRight();
+            Assert.Equal(Orientation.N, robot.Position.Orientation);
+        }
+
+        [Fact]
+        public void WhenRobotRotatesLefItsOrientationChangesAccordingly()
+        {
+            var robot = new Robot(new Position(1, 1, Orientation.N));
+            robot.RotateLeft();
+            Assert.Equal(Orientation.W, robot.Position.Orientation);
+            robot.RotateLeft();
+            Assert.Equal(Orientation.S, robot.Position.Orientation);
+            robot.RotateLeft();
+            Assert.Equal(Orientation.E, robot.Position.Orientation);
+            robot.RotateLeft();
+            Assert.Equal(Orientation.N, robot.Position.Orientation);
+        }
+
+        [Fact]
         public void WhenRobotMovesForwardItDoesSoInTheCorrectOrientation()
         {
             var robot = new Robot(new Position(1, 1, Orientation.N));
